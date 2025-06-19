4075e90b65148cceb5fe6957141a6e4232fb3fdd
ojay432 <davidace998@gmail.com>
2025-06-19 11:28:02 -0400

    updated app.js

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -44,6 +44,7 @@ app.use('/api/gifts', giftRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 //{{insert code here}}
+app.use('/api/search', searchRoutes);app.use('/api/search', searchRoutes);
 
 
 
