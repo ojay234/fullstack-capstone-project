3f9ff60a054865802e0dcd181b1cbd0d334e05fe
ojay432 <davidace998@gmail.com>
2025-06-19 11:21:33 -0400

    serach routes

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -28,6 +28,7 @@ const giftRoutes = require('./routes/giftRoutes');
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 //{{insert code here}}
+const searchRoutes = require('./routes/searchRoutes');
 
 
 const pinoHttp = require('pino-http');
@@ -43,6 +44,7 @@ app.use('/api/gifts', giftRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 //{{insert code here}}
+
 
 
 // Global Error Handler
